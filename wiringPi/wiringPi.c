19fa1f47e0b90ec4aac94df6145caa1f37ddcc4c
AllanBSantos <allanbarrossantos@hotmail.com>
2022-11-04 19:44:06 -0300

    Update wiringPi.c

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -1698,10 +1698,10 @@ void digitalWrite (int pin, int value)
 			else if (wiringPiMode == WPI_MODE_PHYS)
 				pin = physToGpio[pin];
         if(oldPin == 28)
-          pin = 19;
+          pin = 17;
 
         if(oldPin == 31)
-          pin = 8;
+          pin = 20;
           if(-1 == pin) {
         }
 			else 
